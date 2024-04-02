3445ee01ad7b65b239d5587e5a0a41d2536d35fe
Juan Ignacio Campos Martí <104795448+N4choCM@users.noreply.github.com>
2024-04-02 19:44:59 +0200

    ⬆ UPDATED: Formatting in main.hs

--- main.hs
+++ main.hs
@@ -2,25 +2,127 @@ doubleMe x = x + x
 
 doubleUs x y = doubleMe x + doubleMe y
 
-doubleSmallNumber x = if x > 100 then x else x*2
+doubleSmallNumber x = if x > 100 then x else x * 2
 
-doubleSmallNumberPlusOne x = (if x > 100 then x else x*2) + 1
+doubleSmallNumberPlusOne x = (if x > 100 then x else x * 2) + 1
 
-boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 
 customLength xs = sum [1 | _ <- xs]
 
 removeNonUppercase :: [Char] -> [Char]
-removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+removeNonUppercase st = [c | c <- st, c `elem` ['A' .. 'Z']]
 
 addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
 
 factorial :: Integer -> Integer
-factorial n = product [1..n]
+factorial n = product [1 .. n]
 
 circumference :: Float -> Float
 circumference r = 2 * pi * r
 
 circumference' :: Double -> Double
 circumference' r = 2 * pi * r
+
+-- Pattern Matching
+sayMe :: (Integral a) => a -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe 4 = "Four!"
+sayMe 5 = "Five!"
+-- !IMPORTANT: If you add this catch-all pattern at the beginning instead of the end,
+-- !the other patterns will never be checked because this one will always be successful.
+sayMe x = "Entered parameter is not between 1 and 5."
+
+factorialWithPatternMatching :: (Integral a) => a -> a
+factorialWithPatternMatching 0 = 1
+factorialWithPatternMatching n = n * factorialWithPatternMatching (n - 1)
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, y, _) = y
+
+third :: (a, b, c) -> c
+third (_, _, z) = z
+
+head' :: [a] -> a
+head' [] = error "Hey, head cannot be used with an empty list!"
+head' (x : _) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty."
+tell (x : []) = "The list has one element - " ++ show x
+tell (x : y : []) = "The list has two elements - " ++ show x ++ " and " ++ show y
+tell (x : y : _) = "The list is large. The first two elements are " ++ show x ++ " and " ++ show y
+
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_ : xs) = 1 + length' xs
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x : xs) = x + sum' xs
+
+capital :: String -> String
+capital "" = "This is an empty String!"
+capital all@(x : _) = "The first letter of " ++ all ++ " is " ++ [x]
+
+-- Guards
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height
+  | bmi <= skinny = "You're underweight, you emo, you!"
+  | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+  | bmi <= fat = "You are fat! Lose some weight, fatty!"
+  | otherwise = "You're a whale, congratulations!"
+  where
+    bmi = weight / height ^ 2
+    (skinny, normal, fat) = (18.5, 25.0, 30.0)
+
+max' :: (Ord a) => a -> a -> a
+max' a b
+  | a > b = a
+  | otherwise = b
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+  | a > b = GT
+  | a == b = EQ
+  | otherwise = LT
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+  where
+    (f : _) = firstname
+    (l : _) = lastname
+
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h =
+  let sideArea = 2 * pi * r * h
+      topArea = pi * r ^ 2
+   in sideArea + 2 * topArea
+
+head2 :: [a] -> a
+head2 xs = case xs of
+  [] -> error "Hey, head cannot be used with an empty list!"
+  (x : _) -> x
+
+describeList :: [a] -> String
+describeList xs =
+  "The list is" ++ case xs of
+    [] -> "an empty list."
+    [x] -> "a one element list."
+    xs -> "a large list."
+
+describeList' :: [a] -> String
+describeList' xs = "The list is " ++ what xs
+  where
+    what [] = "empty."
+    what [x] = "a singleton list."
+    what xs = "a longer list."
